f7d3fc70d64f022eac23f7711ce60b5155b75baf
ANKIREDDY SAI SHASHANK REDDY <2410030167@klh.edu.in>
2025-11-03 10:22:31 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+import mongoose from "mongodb";
 import profileRoutes from "./routes/profileRoutes.js";
 import searchRoutes from "./routes/searchRoutes.js";
 
